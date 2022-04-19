eec32c281e38835858f2bcdbaa11ddf87239e866
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2022-04-19 18:23:45 +0800

    Added new features
    1. Hover on element via JS added.
    2. Added style (albeit embedded)
    3. Fixed DOM actions.

--- Practice/Form-Retrieval-Sample/script.js
+++ Practice/Form-Retrieval-Sample/script.js
@@ -1,57 +1,87 @@
-console.log("hello doctor...");
-
 const alertsound = new Audio("./PGA1.wav");
 
+//abstracting a long function statement into a simpler one
 function getValueById(elementName){
     return document.getElementById(elementName).value;
 }
 
+//onmouseover();
+function titleHover(){
+    console.log("Hovering at title");
+    document.getElementById("title").style = "color: green";
+}
+
+//onmouseaway();
+function titleAway(){
+    console.log("Away at title");
+    document.getElementById("title").style = "color: black";
+}
+
+//onclick();
 function submitButton(){
     var fname = getValueById("fname");
     var mname = getValueById("mname");
     var lname = getValueById("lname");
+    var age = getValueById("age");
+    var uni = getValueById("university");
 
-    
+    //Sound: https://gomakethings.com/how-to-play-a-sound-with-javascript/
     if(fname.length===0 && mname.length===0 && lname.length===0){
-        //Sound: https://gomakethings.com/how-to-play-a-sound-with-javascript/
+        alertsound.currentTime = 0; //Recommendation: https://stackoverflow.com/a/17636398
         alertsound.play();
         alert("Invalid name input!");
+        return null;
+    }else if(typeof(age) !== "number"){
+        alertsound.currentTime = 0;
+        alertsound.play();
+        alert("Invalid age, try typing a number!");
         return null;
     }
 
     var formalname = lname + ", " + fname + " " + mname[0] + ".";
-    console.log("formalname: " + formalname);
-
-    var age = getValueById("age");
-    console.log("age: " + age)
-
-    var uni = getValueById("university");
-    console.log("university: " + uni);
-
-    let output = "The student's name is " + formalname + " (" + age + ").<br>University of choice: " + uni;
-    console.log(output);
-
-    document.getElementById("output").innerHTML = output;
     
-    //Let's add おいしいくなるもえもえきゅんー！
+    let output = "The student's name is " + formalname + " (" + age + ").<br>University of choice: " + uni;
+    document.getElementById("output").innerHTML = output;
+    console.log(output);
+    
+    var color = "black"; //default color
+    var uniList = [["DLSU","green"], ["ADMU","blue"], ["UP","maroon"], ["UST","yellow"]];
+    for(c of uniList){
+        if(c[0]===uni) //color if specific univeristy has been selected;
+            color = c[1];
+    }
+
+    //Let's add おいしいくなるもえもえきゅんー!;
+    //Tag Specification: <p class="oishii" id="oishii" style="color=color">[CONTENT]</p>
     let oishi = document.createElement("p");
+    oishi.className = "oishii";
     oishi.id = "oishii";
+    oishi.style.color = color;
     let oishi_text = document.createTextNode("おいしいくなるもえもえきゅんー！");
     oishi.appendChild(oishi_text);
     document.getElementsByClassName("output")[0].appendChild(oishi);
 }
 
 function clearButton(){
-    let out = document.getElementById("output")
-    let out2 = document.getElementById("oishii");
-    document.getElementsByClassName("output")[0].removeChild(out); //removes output statement
-    document.getElementsByClassName("output")[0].removeChild(oishii); //removes oishii kunare
+    /*
+    Former implementation: Removes the element itself which means it could not be reused, thus it is more proper to set the innerHTML as empty string instead.
+        let out = document.getElementById("output");
+        document.getElementsByClassName("output")[0].removeChild(out);
+    */
+    let out = document.getElementById("output");
+    out.innerHTML = "";
 
-    document.getElementById("fname").value="";
-    document.getElementById("mname").value="";
-    document.getElementById("lname").value="";
-    document.getElementById("age").value="";
-    document.getElementById("university").value="";
+    let out2 = document.getElementsByClassName("oishii");
+    while(out2.length > 0){ //remove all oishii kunare instances
+        document.getElementsByClassName("output")[0].removeChild(out2[0]);
+    }
+
+    //clear form input data
+    let elemIds = ["fname", "mname", "lname", "age", "university"];
+    for(e of elemIds){
+        document.getElementById(e).value="";
+    }
+    
 
     console.log("clear clicked!");
 }
